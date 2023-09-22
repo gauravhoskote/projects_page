6d023c7b0e575e1cce39edb704712f33377c2a14
Gaurav Hoskote <gaurav.hoskote@gmail.com>
2023-09-21 17:13:51 -0700

    added projects page

---
+++ index.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gaurav Hoskote Projects",
+    # page_icon="👋",
+)
+
+st.write("# Welcome to my Projects Page! 👋")
+
+st.sidebar.success("Projects will be uploaded soon.")
+
+st.markdown(
+    """
+    This website is down for maintenance.
+    I will be uploading my projects on this page soon.
+    Until then, you can take a look at my work experience and read a little bit about the projects I have worked on my [portfolio](https://gauravhoskote.github.io/). 
+"""
+)
+
+# **👈 Select a demo from the sidebar** to see some examples
+#     of what Streamlit can do!
+#     ### Want to learn more?
+#     - Check out [streamlit.io](https://streamlit.io)
+#     - Jump into our [documentation](https://docs.streamlit.io)
+#     - Ask a question in our [community
+#         forums](https://discuss.streamlit.io)
+#     ### See more complex demos
+#     - Use a neural net to [analyze the Udacity Self-driving Car Image
+#         Dataset](https://github.com/streamlit/demo-self-driving)
+#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
